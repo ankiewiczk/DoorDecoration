69150b7a5b5a9a926f23db3cf4bbf675e425613c
Asheton Cady <Ashetonzilla@Gmail.com>
2018-11-07 09:42:25 -0500

    Fixed everything. Integrated Style Changes into main branch.

--- js/flip.js
+++ js/flip.js
@@ -5,6 +5,11 @@ function loadDocument(e) {
     switchEL.onclick = flipIt;
     var panelEL = document.getElementById("flip");
     panelEL.flipped = false;
+	
+	switchEL = document.getElementById("buttonb3");
+    switchEL.onclick = flipb3;
+     panelEL = document.getElementById("b3flap");
+    panelEL.flipped = false;
 }
 
 function flipIt(e) { 
@@ -18,4 +23,16 @@ function flipIt(e) {
         panelEL.flipped = false;
     }
     
+}
+
+function flipb3(e) { 
+    var panelEL = document.getElementById("b3flap");
+
+    if(!panelEL.flipped)  
+    {    panelEL.style.transform = "rotateY(-180deg)";
+        panelEL.flipped = true;
+    } else { 
+        panelEL.style.transform = "";
+        panelEL.flipped = false;
+    }
 }
